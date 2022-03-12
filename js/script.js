1a3801dd843d1b3d399ace8adaf713abf1e14eed
Rakesh Kumar <creativekona@gmail.com>
2022-03-12 13:59:49 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function switchThemeColorDark(){
+	document.getElementById("calc").classList.add("dark");
+	document.getElementById("calc").classList.remove("light");
+}
+
+function switchThemeColorLight(){	
+	document.getElementById("calc").classList.add("light");
+	document.getElementById("calc").classList.remove("dark");
+}
